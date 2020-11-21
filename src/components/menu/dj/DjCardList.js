548b94fd509673e03fc93e3c47007b2e310a1eaf
Cosmin-Stefan Dobranici <cosmin.dobranici16@bathspa.ac.uk>
2020-11-21 20:35:51 +0000

    Fixed mobile css for dj cards

--- src/components/menu/dj/DjCardList.js
+++ src/components/menu/dj/DjCardList.js
@@ -7,12 +7,19 @@ import DjCard from "./DjCard";
 export default function DjCardList({ djList }) {
   return (
     <Container class="justify-content-center">
-      <Row>
-        <Col xs={12} className="dj-card-list">
+      <Row className="desktop">
+        <Col className="dj-card-list">
           {djList.map((djItem) => (
             <DjCard dj={djItem} />
           ))}
         </Col>
+      </Row>
+      <Row className="mobile">
+        {djList.map((djItem) => (
+          <Col xs={12}>
+            <DjCard dj={djItem} />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
