be1660536fba20b83303f2881cfe34b9a8ed1970
Cosmin-Stefan Dobranici <cosmin.dobranici16@bathspa.ac.uk>
2020-12-16 21:53:42 +0000

    fix: fix event list

--- src/components/menu/events/EventList.js
+++ src/components/menu/events/EventList.js
@@ -7,10 +7,12 @@ import "./EventList.css";
 export default function EventList({ eventList }) {
   return (
     <Row className="justify-content-center">
-      <h4>
-        Here at Second Bridge we run only the biggest & best nightlife events in
-        Bath, just take a look!
-      </h4>
+      <Col xs={12} className="justify-content-center">
+        <p style={{ textAlign: "center" }}>
+          Here at Second Bridge we run only the biggest & best nightlife events
+          in Bath, just take a look!
+        </p>
+      </Col>
       <Col xs={12} className="event-list-desktop justify-content-center">
         {eventList.map((eventItem) => (
           <Event event={eventItem} />
